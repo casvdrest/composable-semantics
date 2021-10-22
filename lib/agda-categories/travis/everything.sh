9333a9165f0d125dcc8fa990f36ff7fafa3a0b11
casvdrest <casmb11@xs4all.nl>
2021-10-22 16:27:01 +0200

    clean some

--- lib/agda-categories/travis/everything.sh
+++
@@ -1,7 +0,0 @@
-#/bin/sh
-
-find src/ -name '[^\.]*.agda' \
-    | sed -e 's|^src/[/]*|import |' -e 's|/|.|g' -e 's/.agda//' -e '/import Everything/d' \
-    | LC_COLLATE='C' sort \
-                > Everything.agda
-cat Everything.agda >> index.agda
--- lib/agda-categories/travis/index.agda
+++
@@ -1,6 +0,0 @@
-module index where
-
--- For a brief presentation of every single module, head over to
-import Everything
-
--- Otherwise, here is an exhaustive, stern list of all the available modules.
